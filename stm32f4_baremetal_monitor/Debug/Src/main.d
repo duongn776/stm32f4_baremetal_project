58221335055b5f8a91f9a211b60f9065614c0f92
duongn776 <duongn776@gmail.com>
2025-09-16 23:22:03 +0700

    first commit

---
+++ stm32f4_baremetal_monitor/Debug/Src/main.d
@@ -0,0 +1,3 @@
+Src/main.o: ../Src/main.c \
+ D:/MCU_project/workspace/stm32f4_baremetal_monitor/Drivers/Inc/stm32f407xx.h
+D:/MCU_project/workspace/stm32f4_baremetal_monitor/Drivers/Inc/stm32f407xx.h:
